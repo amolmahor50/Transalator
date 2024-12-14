94262e7feb2dea282ba5e7de6a82ac0adccb7e6a
Amol mahor <amolmahor50@gmail.com>
2024-12-15 00:36:11 +0530

    add more button variable classes

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -13,12 +13,12 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "border border-input bg-background hover:bg-accent hover:text-blue",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
-        blue: "bg-blue-800 hover:bg-blue-400 text-white"
+        blue: "bg-blue-800 hover:bg-blue-600 text-white"
       },
       size: {
         default: "h-10 px-4 py-2",
