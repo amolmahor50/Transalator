5bca159f9cd5ff406b4e1673c16fcb6d9250df00
Amol mahor <amolmahor50@gmail.com>
2025-01-09 00:07:18 +0530

    routing fixed isshue

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
-import HomePage from "./pages/HomePage";
-import { Route, Routes } from "react-router-dom";
-import ImageTranslate from "./routes/ImageTranslate";
-import DocumentTranslate from "./routes/DocumentTranslate";
-import WebsiteTranslate from "./routes/WebsiteTranslate";
-import Text from "./routes/Text";
-import About from "./routes/About";
+import Navbar from "./components/Navbar";
+
 
 export default function App() {
   return (
     <>
-      <HomePage />
-      <div className="max-w-7xl mx-auto px-6">
-        <Routes>
-          <Route path="/" element={<Text />} />
-          <Route path="/about" element={<About/>} />
-          <Route path="/images" element={<ImageTranslate />} />
-          <Route path="/docs" element={<DocumentTranslate />} />
-          <Route path="/websites" element={<WebsiteTranslate />} />
-        </Routes>
-      </div>
+      <Navbar/>
     </>
   )
 }
