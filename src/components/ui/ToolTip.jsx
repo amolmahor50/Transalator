7740bfe0cee16c3ceafe704f7be1126ccaf19d82
Amol mahor <amolmahor50@gmail.com>
2025-01-09 00:14:30 +0530

    tooltip size added

--- src/components/ui/ToolTip.jsx
+++ src/components/ui/ToolTip.jsx
@@ -3,10 +3,10 @@ import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
 import clsx from 'clsx'; // Import clsx for conditional class management
 
-export default function ToolTip({children, TitleToolTip, className = '', ...props}) {
+export default function ToolTip({children, TitleToolTip, size, className = '', ...props}) {
   return (
     <Tooltip title={TitleToolTip}>
-      <IconButton className={clsx('default-classes', className)} {...props}>
+      <IconButton size={size} className={clsx('default-classes', className)} {...props}>
         {children}
       </IconButton>
     </Tooltip>
