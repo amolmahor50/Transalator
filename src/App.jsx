9e7f175608b66c3b2ed657b37bef6dc139667255
Amol mahor <amolmahor50@gmail.com>
2025-01-18 11:12:50 +0530

    next router for outlet add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
 
 export default function App() {
   return (
     <>
-      <Navbar/>
+      <Navbar />
+      <Outlet />
     </>
   )
 }
