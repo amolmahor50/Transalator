470a16c17449bcbe313c66f42f5ba7e30d28d7ea
Amol mahor <amolmahor50@gmail.com>
2025-02-07 16:43:26 +0530

    build ready

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
@@ -9,4 +9,25 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("react") || id.includes("react-dom")) {
+              return "vendor-react"; // React-related libraries
+            }
+            if (id.includes("lodash")) {
+              return "vendor-lodash"; // Lodash in a separate chunk
+            }
+            if (id.includes("firebase")) {
+              return "vendor-firebase"; // Firebase in a separate chunk
+            }
+            return "vendor"; // Other node_modules dependencies
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Adjust this limit if needed
+  },
+});
