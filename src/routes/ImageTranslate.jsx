b2ad78d2a5a9e75de46b071fad99cb8683ead944
Amol mahor <amolmahor50@gmail.com>
2025-02-12 12:30:17 +0530

    images translated ui

--- src/routes/ImageTranslate.jsx
+++ src/routes/ImageTranslate.jsx
@@ -1,15 +1,74 @@
-import { FeedBackLink } from '../components/ui/FeedBackLink';
-import Container from '../components/ui/Container';
-import ManageDataHistory from '../components/ui/DataHistory';
-import React, { useState } from "react";
-import { useDropzone } from "react-dropzone"; // Shadcn/Radix integration for drag and drop
-import ContentPasteIcon from "@mui/icons-material/ContentPaste"; // Icon for "Paste"
-import UploadFileIcon from "@mui/icons-material/UploadFile"; // Icon for drag-and-drop cloud
+import { FeedBackLink } from "../components/ui/FeedBackLink";
+import ManageDataHistory from "../components/ui/DataHistory";
+import React, { useContext, useState } from "react";
+import { useDropzone } from "react-dropzone"; // Drag and Drop integration
+import ContentPasteIcon from "@mui/icons-material/ContentPaste"; // Paste Icon
+import { FaRegCopy } from "react-icons/fa6";
+import { IoClose } from "react-icons/io5";
+import { HiOutlineDownload } from "react-icons/hi";
 import { Button } from "../components/ui/button";
-import { Link } from 'react-router-dom';
-import { Typography } from '@mui/material';
+import { Link } from "react-router-dom";
+import { Box, Typography } from "@mui/material";
+import ToolTip from "../components/ui/ToolTip";
+import Switch from '@mui/material/Switch';
+import { TranslateContextData } from "../context/TranslateContext";
 
-function ImageUploader() {
+const label = { inputProps: { 'aria-label': 'Size switch demo' } };
+console.log(label)
+
+export const ImagePreviewCard = ({ selectedImage, setSelectedImage }) => {
+  return (
+    <div>
+      {/* Header Section */}
+      <div className="flex justify-between items-center w-full py-1 bg-white border-b px-2 sm:border-t-none border-t">
+        <Typography variant="body2" fontWeight="semibold" color="gray" className="flex items-center gap-0 sm:gap-2">
+          Show original
+          <Switch {...label} defaultChecked size="small" />
+        </Typography>
+
+        <div className="flex items-center sm:gap-4">
+          <div className="px-4 py-2 cursor-pointer rounded-sm hover:bg-accent">
+            <Typography variant="body2" fontWeight="semibold" color="gray" className="flex items-center gap-2">
+              <FaRegCopy size={20} />
+              <span className="sm:flex hidden">Copy Text</span>
+            </Typography>
+          </div>
+          <div className="px-4 py-2 cursor-pointer rounded-sm hover:bg-accent">
+            <Typography variant="body2" fontWeight="semibold" color="gray" className="flex items-center gap-2">
+              <HiOutlineDownload size={20} />
+              <span className="sm:flex hidden">Download</span>
+            </Typography>
+          </div>
+          <ToolTip
+            TitleToolTip="Clear Image"
+            onClick={() => setSelectedImage(null)}
+          >
+            <IoClose />
+          </ToolTip>
+        </div>
+      </div>
+
+      {/* Image Preview Section */}
+      <div className='flex justify-center items-center flex-col py-2 bg-accent'>
+        <Typography variant="body2" color="gray">
+          Uploaded File:{" "}
+          <span className="text-blue-600 font-semibold">
+            {selectedImage?.name || "Pasted Image"}
+          </span>
+        </Typography>
+        {selectedImage && (
+          <img
+            src={URL.createObjectURL(selectedImage)}
+            alt="preview"
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default function ImageUploader() {
+  const { sourceText, setSourceText, translatedText, setTranslatedText, sourceLanguage, targetLanguage } = useContext(TranslateContextData);
   const [selectedImage, setSelectedImage] = useState(null);
 
   // Handle file drop functionality using react-dropzone
@@ -35,12 +94,12 @@ function ImageUploader() {
 
   return (
     <>
-      <Container className='sm:h-[300px] h-auto sm:border-2 pb-6 border-b sm:rounded-lg grid sm:grid-cols-12'>
+      <Box className='h-auto sm:border-2 border-b sm:rounded-lg grid sm:grid-cols-2 grid-cols-1'>
         {/* Drag and Drop Box */}
         {!selectedImage && (
           <div
             {...getRootProps()}
-            className={`col-span-6 my-8 p-6 sm:m-16 flex flex-col items-center justify-center`}
+            className={`my-8 p-6 sm:m-16 flex flex-col items-center justify-center`}
           >
             <input {...getInputProps()} />
             <img src="/drag_and_drop.png" className="h-28" />
@@ -52,24 +111,17 @@ function ImageUploader() {
 
         {/* Uploaded File Preview */}
         {selectedImage && (
-          <div className="my-8 col-span-6 flex justify-center items-center flex-col">
-            <p className="text-gray-600">
-              Uploaded File:{" "}
-              <span className='text-blue-600 font-semibold mt-4'>
-                {selectedImage.name || "Pasted Image"}
-              </span>
-            </p>
-            <img
-              src={URL.createObjectURL(selectedImage)}
-              alt="preview"
-              className="mt-4 w-32 h-32 object-cover rounded-md"
+          <div className="col-span-12">
+            <ImagePreviewCard
+              selectedImage={selectedImage}
+              setSelectedImage={setSelectedImage}
             />
           </div>
         )}
 
         {/* File Selection Options */}
         {!selectedImage && (
-          <div className="sm:m-12 flex flex-col sm:border-l-2 items-center col-span-6">
+          <div className="sm:m-12 flex flex-col sm:border-l-2 items-center pb-4 sm:pb-0">
             <p>Or choose a file</p>
 
             {/* Browse Files Button */}
@@ -99,11 +151,10 @@ function ImageUploader() {
             </p>
           </div>
         )}
-      </Container>
+      </Box>
       <FeedBackLink />
       <ManageDataHistory />
     </>
   );
 }
 
-export default ImageUploader;
